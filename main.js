5dacd18b74ff327f82558fc002f95f2a603d0825
Oleg Plaksin <plaksinoleg53@gmail.com>
2023-07-28 18:24:38 +0300

    fix

--- main.js
+++ main.js
@@ -5,40 +5,32 @@ while (counter <= 10) {
 }
 
 const number = 20;
-for (let i = 1; i < number; i += 1) {
+for (let i = 2; i < number; i += 1) {
   if (i % 2 === 1) {
     continue;
   }
   console.log("парне i: ", i);
 }
 
-const numberTwo = 70;
-for (let ii = 7; ii <= numberTwo; ii += 7) {
-    console.log("таблиця множення на 7: ", ii);
+const numberTwo = 7;
+for (let ii = 1; ii <= 10; ii += 1) {
+  res = numberTwo * ii;
+  console.log("таблиця множення на 7: ", res);
 }
 
-const clients = [1];
-while (clients <= 5) {
-    console.log("масив: ", clients[0]);
-    console.log("масив: ", clients[1] = 2)
-    console.log("масив: ", clients[2] = 3)
-    console.log("масив: ", clients[3] = 4)
-    console.log("масив: ", clients[4] = 5)
+const clients = [1, 2, 3, 4, 5];
+let iiiiii = 0;
+while (iiiiii < clients.length) {
+    console.log("масив: ", clients[iiiiii]);
+    iiiiii += 1
 }
 
-for (let iii = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,]; iii.length <= 11;) {
-    console.log("iii: ",iii[0]);
-    console.log("iii: ",iii[1]);
-    console.log("iii: ",iii[2]);
-    console.log("iii: ",iii[3]);
-    console.log("iii: ",iii[4]);
-    console.log("iii: ",iii[5]);
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,];
+for (let iii = 0; numbers.length > iii; iii += 1) {
+    console.log("iii: ",numbers[iii]);
     if (iii[6]) {
      break;
 }
-console.log("iii: ",iii[7]);
-console.log("iii: ",iii[8]);
-console.log("iii: ",iii[9]);
 }
 
 let n = 10;
@@ -50,11 +42,9 @@ for (let iiii = 0; iiii < n; iiii += 1) {
 }
 
 const numberThree = 20;
-
-for (let iiiii = 0; iiiii < number; iiiii += 1) {
+for (let iiiii = 0; iiiii <= numberThree; iiiii += 1) {
   if (iiiii % 3 === 0) {
     continue;
   }
-
   console.log("iiiii: ", iiiii);
 }
